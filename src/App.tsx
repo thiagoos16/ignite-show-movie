4da6758b0abd417a4cbc73bab1b3b2bcc947408c
thiago.oliveira <thiago.oliveira@intera.org.br>
2021-06-15 18:13:31 -0400

    create commons interfaces and add adjusts to use them

--- src/App.tsx
+++ src/App.tsx
@@ -1,31 +1,16 @@
 import { useEffect, useState } from 'react';
 
-import { SideBar } from './components/SideBar';
-import { Content } from './components/Content';
+import { MovieProps, GenreResponseProps } from './interfaces/commons.interfaces';
 
 import { api } from './services/api';
+
+import { SideBar } from './components/SideBar';
+import { Content } from './components/Content';
 
 import './styles/global.scss';
 
 import './styles/sidebar.scss';
 import './styles/content.scss';
-
-interface GenreResponseProps {
-  id: number;
-  name: 'action' | 'comedy' | 'documentary' | 'drama' | 'horror' | 'family';
-  title: string;
-}
-
-interface MovieProps {
-  imdbID: string;
-  Title: string;
-  Poster: string;
-  Ratings: Array<{
-    Source: string;
-    Value: string;
-  }>;
-  Runtime: string;
-}
 
 export function App() {
   const [selectedGenreId, setSelectedGenreId] = useState(1);
